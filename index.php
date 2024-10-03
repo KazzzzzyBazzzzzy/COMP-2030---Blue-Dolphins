3fe9186972b9e26538da5a38358f5552f480d4dd
Kazzzzz <134828453+KazzzzzyBazzzzzy@users.noreply.github.com>
2024-10-03 20:31:23 +0930

    Removed Testing Comment
    Removed comment that said testing

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Bailey Boyd, Anandan Somasundaram, Caleb Kemenyvary, Samuel Ngiri, Abdul Rahaman" />
-    //testing
     <title>SMD Web Solution Index</title>
 </head>
 <body>
