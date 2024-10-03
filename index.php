68f191266735181232176deaf403cecfd5304233
CalebKemenyvary11 <keme0008@flinders.edu.au>
2024-10-03 20:07:16 +0930

    Added name
    Author

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Bailey Boyd, Anandan Somasundaram, " />
+    <meta name="author" content="Bailey Boyd, Anandan Somasundaram, Caleb Kemenyvary" />
     <title>SMD Web Solution Index</title>
 </head>
 <body>
