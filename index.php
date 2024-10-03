c949ea154f9f6119f90da5fcc9cdc99aa5eb9aaa
samuel ngiri <ngir0003@flinders.edu.au>
2024-10-03 20:13:44 +0930

    added name
    added sam

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Bailey Boyd, Anandan Somasundaram, Caleb Kemenyvary" />
+    <meta name="author" content="Bailey Boyd, Anandan Somasundaram, Caleb Kemenyvary, Samuel Ngiri" />
     <title>SMD Web Solution Index</title>
 </head>
 <body>
