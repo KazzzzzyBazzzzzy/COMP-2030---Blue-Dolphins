c9c910c82aa1babb011ce4e51c0ca3966ccd7aa3
Candlekin <emailbaileyboyd@gmail.com>
2024-10-20 17:47:21 +1030

    Added the base task notes framework
    Putting a pause on doing this myself to work on the site map and other report stuff

--- source/production-operator/Task_Notes.php
+++ source/production-operator/Task_Notes.php
@@ -1,21 +1,13 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'productionoperator') {
-    header("Location: ../home/login.php");
-    exit();
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="author" content="Daniel Rosich" />
+    <meta name="author" content="Bailey Boyd, Daniel Rosich" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/global.css">
-    <link rel="stylesheet" type="text/css" href="../css/logout.css">
+    <link rel="stylesheet" href="../style.css">
     <title>Production Operator Home</title>
 </head>
-<h1>Production Operator Home</h1>
+<h1>Add Task Notes</h1>
 <body>
 <div id="production-operator-select">
     <ul>
@@ -23,8 +15,24 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'productionoperator') {
         <li><a href="../home/home.html"><button>Update Machines</button></a></li>
         <li><a href="../home/home.html"><button>Update Jobs</button></a></li>
         <li><a href="../home/home.html"><button>Create Task Notes</button></a></li>
-        <button class="logout-button" onclick="window.location.href='../home/logout.php'">Logout</button>
     </ul>
 </div>
+
+<div id="factory-logs">
+    <h2>Factory Logs</h2>
+    
 </body>
 </html>
+
+<?php
+$host = 'localhost';
+$dbname = 'smd_database';  
+$user = 'root';            
+$pass = '';                
+
+$sql = "SELECT temperature, humidity FROM factory_logs LIMIT 10";
+    $stmt = $pdo->query($sql);
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
