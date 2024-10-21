67c225f42ef07590d3c9ffbc5d64374894046e48
samuel ngiri <ngir0003@flinders.edu.au>
2024-10-22 09:32:51 +1030

    check session
    Check session if session exits

--- source/home/auth_check.php
+++ source/home/auth_check.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 function checkUserRole($requiredRole) {
     if (!isset($_SESSION['user_id']) || $_SESSION['role'] != $requiredRole) {
