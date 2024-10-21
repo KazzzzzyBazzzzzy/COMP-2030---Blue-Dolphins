ad419c9f6f66f014c9aec1cc165f1d2af92cb570
samuel ngiri <ngir0003@flinders.edu.au>
2024-10-22 09:23:32 +1030

    auth check
    auth_check.php will take the expected role as a parameter and check if the current user's session matches that role.

---
+++ source/home/auth_check.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+function checkUserRole($requiredRole) {
+    if (!isset($_SESSION['user_id']) || $_SESSION['role'] != $requiredRole) {
+        header("Location: ../home/login.php");
+        exit();
+    }
+}
+?>
