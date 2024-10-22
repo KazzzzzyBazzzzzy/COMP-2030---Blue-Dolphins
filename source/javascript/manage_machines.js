04b024f8edad7f215313333be5e67f0f17a5a0d5
samuel ngiri <ngir0003@flinders.edu.au>
2024-10-22 14:36:03 +1030

    moving js
    moving js to js folder

---
+++ source/javascript/manage_machines.js
@@ -0,0 +1,5 @@
+function editMachine(id, name) {
+    document.getElementById('machine_id').value = id;
+    document.getElementById('machine_name').value = name;
+    document.getElementById('original_id').value = id;
+}
